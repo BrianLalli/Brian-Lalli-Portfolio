7e96168a8067c88944825f1632983980927b95c1
Brian Lalli <106120015+BrianLalli@users.noreply.github.com>
2022-08-25 17:35:56 -0500

    table setting and react app install and homepage

---
+++ src/components/Home.js
@@ -0,0 +1,228 @@
+import React from "react";
+import Style from "./Home.module.scss";
+import me from "../img/self.png";
+import classNames from "classnames";
+import { Box } from "@mui/material";
+// import {info} from "../../info/Info";
+
+let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+
+const info = {
+  firstName: "Brian",
+  lastName: "Lalli",
+  initials: "bl", // the example uses first and last, but feel free to use three or more if you like.
+  position: "a Program Manager",
+  selfPortrait: "self", // don't change this unless you want to name your self-portrait in the "img" folder something else!
+  gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+  baseColor: colors[0],
+  miniBio: [
+    // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    {
+      emoji: "☕",
+      text: "fueled by coffee",
+    },
+    {
+      emoji: "🌎",
+      text: "based in the US",
+    },
+    {
+      emoji: "💼",
+      text: "Principal Program Manager @ Whole Foods Market",
+    },
+    {
+      emoji: "📧",
+      text: "bdlalli11@gmail.com",
+    },
+  ],
+  socials: [
+    {
+      link: "https://facebook.com",
+      icon: "fa fa-facebook",
+      label: "facebook",
+    },
+    {
+      link: "https://instagram.com",
+      icon: "fa fa-instagram",
+      label: "instagram",
+    },
+    {
+      link: "https://github.com",
+      icon: "fa fa-github",
+      label: "github",
+    },
+    {
+      link: "https://linkedin.com",
+      icon: "fa fa-linkedin",
+      label: "linkedin",
+    },
+    {
+      link: "https://twitter.com",
+      icon: "fa fa-twitter",
+      label: "twitter",
+    },
+    // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+    // Just change the links so that they lead to your social profiles.
+  ],
+  bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+  skills: {
+    proficientWith: [
+      "javascript",
+      "react",
+      "git",
+      "github",
+      "bootstrap",
+      "html5",
+      "css3",
+      "figma",
+    ],
+    exposedTo: ["nodejs", "python", "adobe illustrator"],
+  },
+  hobbies: [
+    {
+      label: "reading",
+      emoji: "📖",
+    },
+    {
+      label: "theater",
+      emoji: "🎭",
+    },
+    {
+      label: "movies",
+      emoji: "🎥",
+    },
+    {
+      label: "cooking",
+      emoji: "🌶",
+    },
+    // Same as above, change the emojis to match / relate to your hobbies or interests.
+    // You can also remove the emojis if you'd like, I just think they look cute :P
+  ],
+  portfolio: [
+    // This is where your portfolio projects will be detailed
+    {
+      title: "Project 1",
+      live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+      source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+      image: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Project 2",
+      live: "https://paytonpierce.dev",
+      source: "https://github.com/paytonjewell",
+      image: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Project 3",
+      live: "https://paytonpierce.dev",
+      source: "https://github.com/paytonjewell",
+      image: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Project 4",
+      live: "https://paytonpierce.dev",
+      source: "https://github.com/paytonjewell",
+      image: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Project 5",
+      live: "https://paytonpierce.dev",
+      source: "https://github.com/paytonjewell",
+      image: "https://via.placeholder.com/150",
+    },
+  ],
+};
+
+function EmojiBullet(props) {
+  const { emoji, text } = props;
+
+  return (
+    <Box
+      component={"li"}
+      fontSize={"1rem"}
+      lineHeight={1.5}
+      style={{ cursor: "default" }}
+    >
+      <Box
+        component={"span"}
+        aria-label="cheese"
+        role="img"
+        mr={{ xs: "0.5rem", md: "1rem" }}
+        fontSize={"1.5rem"}
+      >
+        {emoji}
+      </Box>{" "}
+      {text}
+    </Box>
+  );
+}
+function SocialIcon(props) {
+  const { link, icon, label } = props;
+  return (
+    <a target="_blank" aria-label={label} rel="noopener noreferrer" href={link}>
+      <i className={icon} aria-hidden="true" />
+    </a>
+  );
+}
+
+export default function Home() {
+  return (
+    <Box
+      component={"main"}
+      display={"flex"}
+      flexDirection={{ xs: "column", md: "row" }}
+      alignItems={"center"}
+      justifyContent={"center"}
+      minHeight={"calc(100vh - 175px)"}
+    >
+      <Box
+        className={classNames(Style.avatar, Style.shadowed)}
+        alt={"image of developer"}
+        style={{ background: info.gradient }}
+        component={"img"}
+        src={me}
+        width={{ xs: "35vh", md: "40vh" }}
+        height={{ xs: "35vh", md: "40vh" }}
+        borderRadius={"50%"}
+        p={"0.75rem"}
+        mb={{ xs: "1rem", sm: 0 }}
+        mr={{ xs: 0, md: "2rem" }}
+      />
+      <Box>
+        <h1>
+          Hi, I'm{" "}
+          <span
+            style={{
+              background: info.gradient,
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
+          >
+            {info.firstName}
+          </span>
+          <span className={Style.hand}>🤚</span>
+        </h1>
+        <h2>I'm {info.position}.</h2>
+        <Box component={"ul"} p={"0.8rem"}>
+          {info.miniBio.map((bio, index) => (
+            <EmojiBullet key={index} emoji={bio.emoji} text={bio.text} />
+          ))}
+        </Box>
+        <Box
+          display={"flex"}
+          gap={"1.5rem"}
+          justifyContent={"center"}
+          fontSize={{ xs: "2rem", md: "2.5rem" }}
+        >
+          {info.socials.map((social, index) => (
+            <SocialIcon
+              key={index}
+              link={social.link}
+              icon={social.icon}
+              label={social.label}
+            />
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
+}
